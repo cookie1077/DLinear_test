82a58f1d383ea179996d6eb7c7cac58b9eed652c
cookie1077 <cookie1077@naver.com>
2023-10-28 19:31:58 +0900

    only training

--- FEDformer/scripts/LongForecasting.sh
+++ FEDformer/scripts/LongForecasting.sh
@@ -34,25 +34,6 @@ python -u run.py \
   --checkpoints $checkpoints \
   --itr 1  >$checkpoints/FEDformer_$task_id.log
 
+
 # testing
-python -u run.py \
-  --is_training 0 \
-  --data_path ai_competition_test.csv  \
-  --task_id $task_id\
-  --model FEDformer \
-  --data custom \
-  --features S \
-  --seq_len 60 \
-  --label_len 30 \
-  --pred_len $preLen \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 1 \
-  --dec_in 1 \
-  --c_out 1 \
-  --des 'Exp' \
-  --d_model 512 \
-  --batch_size 8 \
-  --checkpoints $checkpoints \
-  --itr 1  >$checkpoints/FEDformer_$task_id.log
+
