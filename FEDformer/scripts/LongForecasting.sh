b9f19821be9dcf3a35487ed9203faf6cab3729aa
cookie1077 <cookie1077@naver.com>
2023-10-29 01:46:07 +0900

    not happy

--- FEDformer/scripts/LongForecasting.sh
+++ FEDformer/scripts/LongForecasting.sh
@@ -10,13 +10,19 @@ fi
 preLen=30
 task_id=Test1
 checkpoints=/output/
+learning_rate=0.00003
+patience=10
+epochs=50
 
 # training
 python -u run.py \
+  --learning_rate $learning_rate \
+  --train_epochs $epochs \
   --is_training 1 \
   --data_path ai_competition_nonan.csv  \
   --task_id $task_id\
   --model FEDformer \
+  --patience $patience \
   --data custom \
   --features S \
   --seq_len 60 \
@@ -30,7 +36,7 @@ python -u run.py \
   --c_out 1 \
   --des 'Exp' \
   --d_model 512 \
-  --batch_size 8 \
+  --batch_size 2 \
   --checkpoints $checkpoints \
   --itr 1  >$checkpoints/FEDformer_$task_id.log
 
