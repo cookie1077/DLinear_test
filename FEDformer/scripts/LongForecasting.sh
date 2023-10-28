ec927a978186be5e3f2820051a56881b069643ba
cookie1077 <cookie1077@naver.com>
2023-10-28 19:05:50 +0900

    ready to export excel

--- FEDformer/scripts/LongForecasting.sh
+++ FEDformer/scripts/LongForecasting.sh
@@ -11,7 +11,7 @@ preLen=30
 task_id=Test1
 checkpoints=/output/
 
-# ETTm1
+# training
 python -u run.py \
   --is_training 1 \
   --data_path ai_competition_nonan.csv  \
@@ -32,7 +32,27 @@ python -u run.py \
   --d_model 512 \
   --batch_size 8 \
   --checkpoints $checkpoints \
-  --itr 1  >../logs/LongForecasting/FEDformer_$pred_len.log
+  --itr 1  >$checkpoints/FEDformer_$task_id.log
 
-
-# cd ..
+# testing
+python -u run.py \
+  --is_training 0 \
+  --data_path ai_competition_test.csv  \
+  --task_id $task_id\
+  --model FEDformer \
+  --data custom \
+  --features S \
+  --seq_len 60 \
+  --label_len 30 \
+  --pred_len $preLen \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
+  --des 'Exp' \
+  --d_model 512 \
+  --batch_size 8 \
+  --checkpoints $checkpoints \
+  --itr 1  >$checkpoints/FEDformer_$task_id.log
